b7ebcc030f6d041fc3ea7493a54079a92db9a9be
Opilane TTHK <opilane@tthk.ee>
2020-10-23 10:49:04 +0300

    Add project files.

---
+++ Ljalik/Ljalik/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ljalik
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            StackLayout st = new StackLayout();
+            Button esm_btn = new Button { Text = "Esmaspäev" };
+
+            st.Children.Add(esm_btn);
+            esm_btn.Clicked += new EventHandler(Btns_Click);
+            Content = st;
+        }
+
+        private async void Btns_Click(object sender, EventArgs e)
+        {
+            var btn = (Button)sender;
+            switch (btn.Text)//проверяем текст на кнопке
+            {
+                case "Esmaspäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Esmaspaev());//открывается esmaspaev
+                    break;
+                case "Teisipäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Teisipaev());//открывается Esmaspaev
+                    break;
+
+
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
