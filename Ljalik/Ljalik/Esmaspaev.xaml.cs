5cb210518a23d0738aac9cb188f676c0f829b4f2
opilane <opilane@HP-CZC6297B92>
2020-10-23 12:24:00 +0300

    Ljalik

--- Ljalik/Ljalik/Esmaspaev.xaml.cs
+++ Ljalik/Ljalik/Esmaspaev.xaml.cs
@@ -14,7 +14,22 @@ namespace Ljalik
     {
         public Esmaspaev()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            //массив план на день
+            string[] tasks = new string[] { "Подъем", "Умывание", "Прогулка", "Прием пищи", "Поиграть в комп", "Спать" };
+            ListView List = new ListView();//список для отражения значений массива
+            List.ItemsSource = tasks;//данные из массива отображаем в списке
+            List.ItemSelected += List_ItemSelected;//инициализирукем функцию при клике на пункты списка
+            Content = new StackLayout { Children = { List } };//отображем список га экране
+        }
+
+        private void List_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+           if (e.SelectedItem !=null)
+            {
+                string text = e.SelectedItem.ToString();
+                if (e.SelectedItem==0)//если выбран
+            }
         }
     }
 }
--- Ljalik/Ljalik/MainPage.xaml.cs
+++ Ljalik/Ljalik/MainPage.xaml.cs
@@ -15,9 +15,26 @@ namespace Ljalik
             InitializeComponent();
             StackLayout st = new StackLayout();
             Button esm_btn = new Button { Text = "Esmaspäev" };
-
             st.Children.Add(esm_btn);
             esm_btn.Clicked += new EventHandler(Btns_Click);
+            Button tsp_btn = new Button { Text = "Teisipäev" };
+            st.Children.Add(tsp_btn);
+            tsp_btn.Clicked += new EventHandler(Btns_Click);
+            Button kol_btn = new Button { Text = "Kolmapäev" };
+            st.Children.Add(kol_btn);
+            kol_btn.Clicked += new EventHandler(Btns_Click);
+            Button nel_btn = new Button { Text = "Neljapäev" };
+            st.Children.Add(nel_btn);
+            nel_btn.Clicked += new EventHandler(Btns_Click);
+            Button red_btn = new Button { Text = "Reede" };
+            st.Children.Add(red_btn);
+            red_btn.Clicked += new EventHandler(Btns_Click);
+            Button lul_btn = new Button { Text = "Laupäev" };
+            st.Children.Add(lul_btn);
+            lul_btn.Clicked += new EventHandler(Btns_Click);
+            Button puh_btn = new Button { Text = "Pühapäev" };
+            st.Children.Add(puh_btn);
+            puh_btn.Clicked += new EventHandler(Btns_Click);
             Content = st;
         }
 
@@ -32,10 +49,28 @@ namespace Ljalik
                     break;
                 case "Teisipäev":
                     await Navigation.PopAsync();
-                    await Navigation.PushAsync(new Teisipaev());//открывается Esmaspaev
+                    await Navigation.PushAsync(new Teisipaev());//открывается Teisipaev
                     break;
-
-
+                case "Kolmapäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Kolmapaev());//открывается Kolmapaev
+                    break;
+                case "Neljapäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Neljapaev());//открывается Neljapäev
+                    break;
+                case "Reede":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Reede());//открывается Reede
+                    break;
+                case "Laupäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Teisipaev());//открывается Laupaev
+                    break;
+                case "Pühapäev":
+                    await Navigation.PopAsync();
+                    await Navigation.PushAsync(new Teisipaev());//открывается Puhapaev
+                    break;
                     throw new NotImplementedException();
             }
         }
