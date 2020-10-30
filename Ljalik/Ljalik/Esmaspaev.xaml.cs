10cf8dece965091501e22ca0c2eb173e8664ffff
opilane <opilane@HP-CZC6297B92>
2020-10-30 11:24:06 +0200

    Ljalik

--- Ljalik/Ljalik/Esmaspaev.xaml.cs
+++ Ljalik/Ljalik/Esmaspaev.xaml.cs
@@ -96,5 +96,6 @@ namespace Ljalik
         {
             await Navigation.PopAsync();
         }
+
     }
 }
--- Ljalik/Ljalik/Neljapaev.xaml.cs
+++ Ljalik/Ljalik/Neljapaev.xaml.cs
@@ -15,6 +15,68 @@ namespace Ljalik
         public Neljapaev()
         {
             InitializeComponent();
+            string[] tasks = new string[] { "Пробудился", "Позавтракал", "Стоял на посту", "Пошел обедать", "Отдыхаю", "Играю в карты", "Ужинаю", "Ложусь спать" };
+            ListView list = new ListView { BackgroundColor = Color.WhiteSmoke, SeparatorColor = Color.Gray, RowHeight = 50, };
+            list.ItemsSource = tasks;
+            Button bb = new Button { Text = "Назад" };
+            bb.Clicked += Bb_Clicked;
+            Button bbk = new Button { Text = "Вперед" };
+            bbk.Clicked += Bbk_Clicked;
+            list.ItemSelected += List_ItemSelected;
+            Content = new StackLayout { Children = { list, bb, bbk } };
         }
+
+        private async void Bbk_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Reede());
+        }
+
+        private async void Bb_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private async void List_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            string text = e.SelectedItem.ToString();
+            if (e.SelectedItemIndex == 0)
+            {
+                Time = "9:24"; //Проснулся
+            }
+            if (e.SelectedItemIndex == 1)
+            {
+                Time = "10:00";//завтрак
+            }
+
+            if (e.SelectedItemIndex == 2)
+            {
+                Time = "12:00";//атака
+            }
+
+            if (e.SelectedItemIndex == 3)
+            {
+                Time = "14:00";//отдых
+            }
+
+            if (e.SelectedItemIndex == 4)
+            {
+                Time = "18:00";//чистка
+            }
+
+            if (e.SelectedItemIndex == 5)
+            {
+                Time = "19:00";//ужин
+            }
+
+            if (e.SelectedItemIndex == 6)
+            {
+                Time = "22:00";//ложусь спать
+            }
+            await DisplayAlert(text, Time, "Продолжить");
+        }
+
+        string Time;
+
     }
+
 }
